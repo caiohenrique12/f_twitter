059e3578586e0c028832fe1ca7ecf6f38759d63d
Caio H <caiohenriqueads@gmail.com>
2017-03-10 14:08:12 -0300

    testando e implementando informações em home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @posts = Post.find_by_user_id(current_user.id)
+    @posts = Post.find_by_post(current_user.id)
   end
 end
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "home/index", type: :view do
+
+  before(:each) do
+    assign(:users, [
+      @user = User.create!(
+        :name => 'Lilyan M',
+        :email => 'lilya.m@gmail.com',
+        :password => '123456',
+        :password_confirmation => '123456'
+      )
+    ])
+  end
+
+  before(:each) do
+    assign(:posts, [
+      Post.create!(
+        :title => "MyString",
+        :text => "MyString",
+        :user_id => @user.id
+      ),
+      Post.create!(
+        :title => "MyString",
+        :text => "MyString",
+        :user_id => @user.id
+      )
+    ])
+  end
+
+  it "renders a list of posts" do
+    render
+    assert_select "tr>td", :text => "MyString".to_s, :count => 2
+    assert_select "tr>td", :text => "MyString".to_s, :count => 2
+    assert_select "tr>td", :text => @user.id.to_s, :count => 2
+  end
+
 end
