18faf092ac5eb5b9d933c49055cdc6b0d7092855
Caio H <caiohenriqueads@gmail.com>
2017-03-10 09:08:38 -0300

    corrigindo nome da variavel

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :text, :user)
+      params.require(:post).permit(:title, :text, :user_id)
     end
 end
